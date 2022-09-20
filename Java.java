85b10fbc749759662ccc0346254949024edc643f
mshidayat66 <mshidayat66@gmail.com>
2022-09-20 22:50:15 +0700

    Muhammad Syarif Hidayat

---
+++ Java.java
@@ -0,0 +1,8 @@
+
+public class Java {
+    public static void main(String[]argas){
+        System.out.println("Belajar JAVA");
+        System.out.println("Sangat mudah sekali");
+        System.out.println("dan sangat menyenangkan");
+    }
+}
---
+++ No2.java
@@ -0,0 +1,10 @@
+
+import javax.swing.JOptionPane;
+
+
+public class No2 {
+    public static void main(String[] args) {
+        String kata = JOptionPane.showInputDialog("Anda sedang belajar apa?");
+        JOptionPane.showMessageDialog(null, "Belajar "+ kata + " sangat mudah");
+    }
+}
